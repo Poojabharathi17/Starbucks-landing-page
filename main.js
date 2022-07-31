bb2f26d0699b21a1131a814450a2d2438f9496fe
Pooja bharathi. P <poojabharathi178@gmail.com>
2022-07-31 20:09:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ function toggleMenu(){
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('active')
 }
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
